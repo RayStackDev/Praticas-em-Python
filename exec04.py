aa1b75d0dea88a863c22a3dae03c1ef12eb51c30
Raymond <raymondlugo64@gmail.com>
2025-09-11 15:12:57 -0400

    Programa que lê um numero positivo e mostra ele ao quadrado e a raiz quadrada, caso for negativo, nao sera possivel seguir com a operaçao.

---
+++ exec04.py
@@ -0,0 +1,7 @@
+number1 = int(input("Digite um numero: "))
+
+if number1 > 0 :
+    print(f"O numero {number1} ao quadrado é: {number1**2}\n")
+    print(f"A raiz quadrado de {number1} é: {number1**0.5}")
+else:
+    print("Erro! Nao pode ser numero negativo.")
